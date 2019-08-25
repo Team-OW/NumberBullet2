aa8931ee783c631d491c2b2c81ea56f37c90ee94
Aoi Takahashi <aoi.t19950517@gmail.com>
2019-08-25 17:05:48 +0900

    Revert "記録画面のリファクタリングと描画時の非同期処理の検討"

--- Assets/Project/Scripts/RecordScene/RecordDirector.cs
+++ Assets/Project/Scripts/RecordScene/RecordDirector.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using Project.Scripts.Utils.Definitions;
 using Project.Scripts.Utils.PlayerPrefsUtils;
@@ -17,100 +16,56 @@ namespace Project.Scripts.RecordScene
 
         [SerializeField] private GameObject _successLinePrefab;
 
-        private readonly Dictionary<EStageLevel, GameObject> _levelText = new Dictionary<EStageLevel, GameObject>();
+        private readonly Dictionary<EStageLevel, GameObject> levelText = new Dictionary<EStageLevel, GameObject>();
 
-        private readonly Dictionary<EStageLevel, GameObject> _percentageText = new Dictionary<EStageLevel, GameObject>();
+        private readonly Dictionary<EStageLevel, GameObject> percentageText = new Dictionary<EStageLevel, GameObject>();
 
-        private readonly Dictionary<EStageLevel, GameObject> _graphArea = new Dictionary<EStageLevel, GameObject>();
+        private readonly Dictionary<EStageLevel, GameObject> graphArea = new Dictionary<EStageLevel, GameObject>();
 
-        private SnapScrollView _snapScrollView;
-
-        /* 以下，グラフ描画 UI 周りの配置設定 */
-
-        // 棒グラフ描画範囲の左端
-        private const float LEFT_POSITION = 0.1f;
-
-        // 棒グラフ描画範囲の右端
-        private const float RIGHT_POSITION = 0.95f;
-
-        // 棒グラフ本体の上端
-        private const float TOP_POSITION = 0.9f;
-
-        // 棒グラフ本体の下端
-        private const float BOTTOM_POSITION = 0.15f;
-
-        // 棒グラフの横幅が隙間の何倍か
-        private const float GRAPH_WIDTH_RATIO = 1.0f;
-
-        // ステージ番号の下端
-        private const float BOTTOM_STAGE_NUM_POSITION = 0.05f;
-
-        // 成功している場合のグラフの色
-        [SerializeField] private Color _successGraphColor = Color.cyan;
-
-        // 成功していない場合のグラフの色
-        [SerializeField] private Color _notSuccessGraphColor = Color.red;
+        private SnapScrollView snapScrollView;
 
         private void Awake()
         {
             // 取得
-            _snapScrollView = FindObjectOfType<SnapScrollView>();
+            snapScrollView = GameObject.Find("SnapScrollView").GetComponent<SnapScrollView>();
             // ページの最大値を設定
-            _snapScrollView.MaxPage = Enum.GetNames(typeof(EStageLevel)).Length - 1;
+            snapScrollView.MaxPage = Enum.GetNames(typeof(EStageLevel)).Length - 1;
             // ページの横幅の設定
-            _snapScrollView.PageSize = Screen.width;
+            snapScrollView.PageSize = Screen.width;
 
+            // TODO: 将来的には非同期で呼び出したい (バージョンアップ待ち)
             // 各種グラフなどを全て描画する
             Draw();
         }
 
-        /// <summary>
-        /// 全難易度の画面を描画する
-        /// </summary>
+        /* 全難易度の画面を描画する */
         private void Draw()
         {
-            foreach (EStageLevel stageLevel in Enum.GetValues(typeof(EStageLevel))) StartCoroutine(DrawEach(stageLevel));
+            foreach (EStageLevel stageLevel in Enum.GetValues(typeof(EStageLevel))) {
+                // 準備
+                GetGameObjects(stageLevel);
+                // 成功割合の描画
+                DrawPercentage(stageLevel);
+                // 棒グラフの描画
+                DrawGraph(stageLevel);
+                // タイトルの変更
+                levelText[stageLevel].GetComponent<Text>().text = StageInfo.LevelName[stageLevel];
+            }
         }
 
-        /// <summary>
-        /// 各難易度の画面を描画する
-        /// </summary>
-        /// <param name="stageLevel"> 難易度 </param>
-        private IEnumerator DrawEach(EStageLevel stageLevel)
-        {
-            // GameObject の準備
-            GetGameObjects(stageLevel);
-            // タイトルの変更
-            _levelText[stageLevel].GetComponent<Text>().text = StageInfo.LevelName[stageLevel];
-            // 成功割合の描画
-            DrawPercentage(stageLevel);
-            // 棒グラフの描画
-            DrawGraph(stageLevel);
-
-            yield return null;
-        }
-
-        /// <summary>
-        /// 各難易度に必要な GameObject を取得
-        /// </summary>
-        /// <param name="stageLevel"> 難易度 </param>
+        /* 必要な GameObject を Scene から取得 */
         private void GetGameObjects(EStageLevel stageLevel)
         {
-            // Canvas -> SnapScrollView -> Viewport -> Content -> ${stageLevel} を取得
-            var level = GameObject.Find(stageLevel.ToString()).transform;
+            // SnapScrollView -> Viewport -> Content -> のオブジェクトを特定
+            var level = GameObject.Find(stageLevel.ToString());
 
-            // ${stageLevel} -> Level を取得
-            _levelText.Add(stageLevel, level.Find("Level").gameObject);
-            // ${stageLevel} -> Percentage を取得
-            _percentageText.Add(stageLevel, level.Find("Percentage").gameObject);
-            // ${stageLevel} -> GraphArea を取得
-            _graphArea.Add(stageLevel, level.Find("GraphArea").gameObject);
+            // 各種 GameObject を取得
+            levelText.Add(stageLevel, level.transform.Find("Level").gameObject);
+            percentageText.Add(stageLevel, level.transform.Find("Percentage").gameObject);
+            graphArea.Add(stageLevel, level.transform.Find("GraphArea").gameObject);
         }
 
-        /// <summary>
-        /// 難易度に合わせた成功割合を描画する
-        /// </summary>
-        /// <param name="stageLevel"> 難易度 </param>
+        /* 難易度に合わせた成功割合を描画する */
         private void DrawPercentage(EStageLevel stageLevel)
         {
             var stageNum = StageInfo.Num[stageLevel];
@@ -128,44 +83,61 @@ namespace Project.Scripts.RecordScene
 
             var successPercentage = (successStageNum / (float) stageNum) * 100;
 
-            _percentageText[stageLevel].GetComponent<Text>().text = successPercentage + "%";
+            percentageText[stageLevel].GetComponent<Text>().text = successPercentage + "%";
         }
 
-        /// <summary>
-        /// 難易度に合わせた棒グラフを描画する
-        /// </summary>
-        /// <param name="stageLevel"> 難易度 </param>
+        /* 難易度に合わせた棒グラフを描画する */
         private void DrawGraph(EStageLevel stageLevel)
         {
             var stageNum = StageInfo.Num[stageLevel];
             var stageStartId = StageInfo.StageStartId[stageLevel];
 
             // 描画するパネル
-            var graphAreaContent = _graphArea[stageLevel].GetComponent<RectTransform>();
+            var graphAreaContent = graphArea[stageLevel].GetComponent<RectTransform>();
+
+            /* UIの配置周りの定数宣言 */
+
+            // 棒グラフ描画範囲の左端
+            const float leftPosition = 0.1f;
+
+            // 棒グラフ描画範囲の右端
+            const float rightPosition = 0.95f;
+
+            // 棒グラフ本体の上端
+            const float topPosition = 0.9f;
+
+            // 棒グラフ本体の下端
+            const float bottomPosition = 0.15f;
+
+            // 棒グラフの横幅が隙間の何倍か
+            const float graphWidthRatio = 1.0f;
+
+            // ステージ番号の下端
+            const float bottomStageNumPosition = 0.05f;
 
             // グラフ間の隙間の横幅 -> stageNum個のグラフと(stageNum + 1)個の隙間
-            var blankWidth = (RIGHT_POSITION - LEFT_POSITION) / (stageNum * GRAPH_WIDTH_RATIO + (stageNum + 1));
+            var blankWidth = (rightPosition - leftPosition) / (stageNum * graphWidthRatio + (stageNum + 1));
 
             // 棒グラフの横幅
-            var graphWidth = blankWidth * GRAPH_WIDTH_RATIO;
+            var graphWidth = blankWidth * graphWidthRatio;
 
             // 挑戦回数の最大値を求める
             var maxChallengeNum = GetMaxChallengeNum(stageNum, stageStartId);
 
-            // 挑戦回数の最大値に収まる 30 で割れる目盛の最小値を求める
+            // 目盛の最大値を求める
             var maxScale = (float) Math.Ceiling((float) maxChallengeNum / 30) * 30;
 
             // 目盛を書き換える
             if (maxScale > 0) {
-                _graphArea[stageLevel].transform.Find("Scale4-Value").gameObject.GetComponent<Text>().text = maxScale.ToString();
-                _graphArea[stageLevel].transform.Find("Scale3-Value").gameObject.GetComponent<Text>().text = (maxScale * 2 / 3).ToString();
-                _graphArea[stageLevel].transform.Find("Scale2-Value").gameObject.GetComponent<Text>().text = (maxScale / 3).ToString();
+                graphArea[stageLevel].transform.Find("Scale4-Value").gameObject.GetComponent<Text>().text = maxScale.ToString();
+                graphArea[stageLevel].transform.Find("Scale3-Value").gameObject.GetComponent<Text>().text = (maxScale * 2 / 3).ToString();
+                graphArea[stageLevel].transform.Find("Scale2-Value").gameObject.GetComponent<Text>().text = (maxScale / 3).ToString();
             }
 
             // ステージ番号
             var stageName = 1;
             // 描画する棒グラフの左端を示す
-            var left = LEFT_POSITION;
+            var left = leftPosition;
 
             for (var stageId = stageStartId; stageId < stageStartId + stageNum; stageId++) {
                 // 左端と右端の更新
@@ -178,37 +150,37 @@ namespace Project.Scripts.RecordScene
                 var stageNumUi = Instantiate(_stageNumPrefab);
                 stageNumUi.transform.SetParent(graphAreaContent, false);
                 stageNumUi.GetComponent<Text>().text = stageName.ToString();
-                stageNumUi.GetComponent<RectTransform>().anchorMin = new Vector2(left, BOTTOM_STAGE_NUM_POSITION);
-                stageNumUi.GetComponent<RectTransform>().anchorMax = new Vector2(right, BOTTOM_POSITION);
+                stageNumUi.GetComponent<RectTransform>().anchorMin = new Vector2(left, bottomStageNumPosition);
+                stageNumUi.GetComponent<RectTransform>().anchorMax = new Vector2(right, bottomPosition);
 
                 /* 棒グラフの配置 */
                 var graphUi = Instantiate(_graphPrefab);
                 graphUi.transform.SetParent(graphAreaContent, false);
 
                 // 挑戦回数に応じた棒グラフの上端
-                var graphMaxY = BOTTOM_POSITION;
+                var graphMaxY = bottomPosition;
 
                 if (maxChallengeNum != 0) {
                     // 挑戦回数を用いて，棒グラフの高さを描画範囲に正規化する
-                    graphMaxY = (TOP_POSITION - BOTTOM_POSITION) * (stageStatus.challengeNum / maxScale) + BOTTOM_POSITION;
+                    graphMaxY = (topPosition - bottomPosition) * (stageStatus.challengeNum / maxScale) + bottomPosition;
                 }
 
-                graphUi.GetComponent<RectTransform>().anchorMin = new Vector2(left, BOTTOM_POSITION);
+                graphUi.GetComponent<RectTransform>().anchorMin = new Vector2(left, bottomPosition);
                 graphUi.GetComponent<RectTransform>().anchorMax = new Vector2(right, graphMaxY);
 
                 if (stageStatus.passed) {
                     /* 成功している場合は，色を水色にして，成功した際の挑戦回数も示す */
-                    graphUi.GetComponent<Image>().color = _successGraphColor;
+                    graphUi.GetComponent<Image>().color = Color.cyan;
 
                     var successLineUi = Instantiate(_successLinePrefab);
                     successLineUi.transform.SetParent(graphAreaContent, false);
-                    var successY = (TOP_POSITION - BOTTOM_POSITION) * (stageStatus.firstSuccessNum / maxScale) +
-                        BOTTOM_POSITION;
+                    var successY = (topPosition - bottomPosition) * (stageStatus.firstSuccessNum / maxScale) +
+                        bottomPosition;
                     successLineUi.GetComponent<RectTransform>().anchorMin = new Vector2(left, successY);
                     successLineUi.GetComponent<RectTransform>().anchorMax = new Vector2(right, successY);
                 } else {
                     /* 成功していない場合は，色を赤色にする */
-                    graphUi.GetComponent<Image>().color = _notSuccessGraphColor;
+                    graphUi.GetComponent<Image>().color = Color.red;
                 }
 
                 // 左端の更新
@@ -218,12 +190,7 @@ namespace Project.Scripts.RecordScene
             }
         }
 
-        /// <summary>
-        /// 挑戦回数の最大値を求める
-        /// </summary>
-        /// <param name="stageNum"></param>
-        /// <param name="stageStartId"></param>
-        /// <returns></returns>
+        /* 挑戦回数の最大値を求める */
         private static int GetMaxChallengeNum(int stageNum, int stageStartId)
         {
             var maxChallengeNum = 0;
